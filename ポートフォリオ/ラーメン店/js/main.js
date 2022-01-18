246accbdd4e2b4f9e9f8c5c0688f941ecea62c77
leno41bo52ru <leno41bo52ru@gmail.com>
2022-01-18 11:38:19 +0900

    content01まで作成

--- ポートフォリオ/ラーメン店/js/main.js
+++ ポートフォリオ/ラーメン店/js/main.js
@@ -4,8 +4,14 @@ let mySwiper = new Swiper ('.swiper', {
   // 以下にオプションを設定
   loop: true,
   speed: 500,
-  slidesPerView: 2,
+  slidesPerView: 1,
   centeredSlides : true,
+  //▼pc版▼
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
  
   //ページネーション表示の設定
   pagination: { 
@@ -24,4 +30,24 @@ let mySwiper = new Swiper ('.swiper', {
   // scrollbar: { 
   //   el: '.swiper-scrollbar', //要素の指定
   // },
+});
+
+//同じ記述なのでリファクタリングしたい
+//ハンバーガーメニュー×押したらメニュー閉じる
+$(function () {
+  $('.burger-btn').on('click', function () {
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').toggleClass('fade');
+    $('body').toggleClass('fixed');
+    $('body').toggleClass('noscroll');
+  });
+});
+//ハンバーガーメニューリンククリックしたらメニュー閉じる
+$(function () {
+  $('.nav-item').on('click', function () {
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').toggleClass('fade');
+    $('body').removeClass('fixed');
+    $('body').removeClass('noscroll');
+  });
 });
