90dea6e963910861e7810ca0ef7bcf72cad12a28
leno41bo52ru <leno41bo52ru@gmail.com>
2022-02-15 10:35:48 +0900

    申し込みフォーム作成

--- ポートフォリオ/ラーメン店/js/main.js
+++ ポートフォリオ/ラーメン店/js/main.js
@@ -159,3 +159,28 @@ $('.gallery').on('beforeChange', function(event, slick, currentSlide, nextSlide)
 	//サムネイルのslick-currentを削除し次のスライド要素にslick-currentを追加
 	$(".choice-btn .slick-slide").removeClass("slick-current").eq(index).addClass("slick-current");
 });
+
+//フォームテキストファイルに出力
+function dispText() {
+  var text = "'" +
+      document.formname.name01.value + "','" +
+      document.formname.name02.value + "','" +
+      document.formname.year.value + "','" +
+      document.formname.month.value + "','" +
+      document.formname.day.value + "','" +
+      document.formname.gender.value + "','" +
+      document.formname.email.value + "','" +
+      document.formname.tell.value + "','" +
+      document.formname.job.value + "','" +
+      document.formname.address01.value + "','" +
+      document.formname.address02.value + "'";
+  var blob = new Blob([text], { "type": "text/plain" });
+
+  //IEの場合
+  if (window.navigator.msSaveBlob) {
+      window.navigator.msSaveBlob(blob, "outFileFromWindows.txt");
+  //IE以外の場合
+  } else {
+      document.getElementById("createFile").href = window.URL.createObjectURL(blob);
+  }
+}
