53fdf25b319d15bd41aa97af02c3452823e05d76
leno41bo52ru <leno41bo52ru@gmail.com>
2022-02-17 18:43:38 +0900

    全体微修正 フォーム部分はサーバーにアップロードしてから作成しようと思います。

--- ポートフォリオ/ラーメン店/js/job/form.js
+++ ポートフォリオ/ラーメン店/js/job/form.js
@@ -1,24 +1,25 @@
 //フォームテキストファイルに出力
 function dispText() {
-  var text = "'" +
-      document.formname.name01.value + "','" +
-      document.formname.name02.value + "','" +
-      document.formname.year.value + "','" +
-      document.formname.month.value + "','" +
-      document.formname.day.value + "','" +
-      document.formname.gender.value + "','" +
-      document.formname.email.value + "','" +
-      document.formname.tell.value + "','" +
-      document.formname.job.value + "','" +
-      document.formname.address01.value + "','" +
-      document.formname.address02.value + "'";
-  var blob = new Blob([text], { "type": "text/plain" });
+  console.log("確認画面を押しました。")
+  // var text = "'" +
+  //     document.formname.name01.value + "','" +
+  //     document.formname.name02.value + "','" +
+  //     document.formname.year.value + "','" +
+  //     document.formname.month.value + "','" +
+  //     document.formname.day.value + "','" +
+  //     document.formname.gender.value + "','" +
+  //     document.formname.email.value + "','" +
+  //     // document.formname.tell.value + "','" +
+  //     document.formname.job.value + "','" +
+  //     document.formname.address01.value + "','" +
+  //     document.formname.address02.value + "'";
+  // var blob = new Blob([text], { "type": "text/plain" });
 
-  //IEの場合
-  if (window.navigator.msSaveBlob) {
-      window.navigator.msSaveBlob(blob, "outFileFromWindows.txt");
-  //IE以外の場合
-  } else {
-      document.getElementById("createFile").href = window.URL.createObjectURL(blob);
-  }
+  // //IEの場合
+  // if (window.navigator.msSaveBlob) {
+  //     window.navigator.msSaveBlob(blob, "outFileFromWindows.txt");
+  // //IE以外の場合
+  // } else {
+  //     document.getElementById("createFile").href = window.URL.createObjectURL(blob);
+  // }
 }
