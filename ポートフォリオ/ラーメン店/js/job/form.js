a40fa5367fb0ebdb88c4643b9f93ec43571a2431
leno41bo52ru <leno41bo52ru@gmail.com>
2022-02-22 17:48:26 +0900

    フォームフェードイン、フェードアウトまで作成。 ▼残作業 フォームのバリデーションエラー 送信ボタン先ページ作成

--- ポートフォリオ/ラーメン店/js/job/form.js
+++ ポートフォリオ/ラーメン店/js/job/form.js
@@ -1,15 +1,18 @@
-//フォームテキストファイルに出力
 function ConfirmForm() {
   console.log("確認画面を押しました。");
+  var formConf_elm = document.getElementById("formConf-fadeIn");
+  formConf_elm.classList.add('show');
+  document.body.classList.add('fixed','noscroll');
 
+  //フォーム変数定義
   const name01 = document.formname.name01.value;
   const name02 = document.formname.name02.value;
   const year = document.formname.year.value;
   const month = document.formname.month.value;
   const day = document.formname.day.value;
-  let genderRadio = document.getElementsByName('gender');
-  let len = genderRadio.length;
-  let checkValue = '';
+  const genderRadio = document.getElementsByName('gender');
+  const len = genderRadio.length;
+  let gender = '';
   for (let i = 0; i < len; i++){
     if (genderRadio.item(i).checked){
       gender = genderRadio.item(i).value;
@@ -31,29 +34,42 @@ function ConfirmForm() {
   console.log("job=" + job);
   console.log("address01=" + address01);
   console.log("address02=" + address02);
-  window.open('../index.html', '_blank');//別タブで開く
-  // window.location.href = '../index.html';
 
+  if(name01 === "") {
+    console.log("name01が入力されていません");
+  }
 
-  // var text = "'" +
-  //     document.formname.name01.value + "','" +
-  //     document.formname.name02.value + "','" +
-  //     document.formname.year.value + "','" +
-  //     document.formname.month.value + "','" +
-  //     document.formname.day.value + "','" +
-  //     document.formname.gender.value + "','" +
-  //     document.formname.email.value + "','" +
-  //     // document.formname.tell.value + "','" +
-  //     document.formname.job.value + "','" +
-  //     document.formname.address01.value + "','" +
-  //     document.formname.address02.value + "'";
-  // var blob = new Blob([text], { "type": "text/plain" });
+  // window.open('../index.html', '_blank');//別タブで開く
 
-  // //IEの場合
-  // if (window.navigator.msSaveBlob) {
-  //     window.navigator.msSaveBlob(blob, "outFileFromWindows.txt");
-  // //IE以外の場合
-  // } else {
-  //     document.getElementById("createFile").href = window.URL.createObjectURL(blob);
-  // }
+  //確認ページの要素取得
+  var name01_elm = document.getElementById("name01_Conf");
+  var name02_elm = document.getElementById("name02_Conf");
+  var year_elm = document.getElementById("year_Conf");
+  var month_elm = document.getElementById("month_Conf");
+  var day_elm = document.getElementById("day_Conf");
+  var gender_elm = document.getElementById("gender_Conf");
+  var email_elm = document.getElementById("email_Conf");
+  var tel_elm = document.getElementById("tel_Conf");
+  var job_elm = document.getElementById("job_Conf");
+  var address01_elm = document.getElementById("address01_Conf");
+  var address02_elm = document.getElementById("address02_Conf");
+
+  //確認ページ値代入
+  name01_elm.innerHTML = name01;
+  name02_elm.innerHTML = name02;
+  year_elm.innerHTML = year;
+  month_elm.innerHTML = month;
+  day_elm.innerHTML = day;
+  gender_elm.innerHTML = gender;
+  email_elm.innerHTML = email;
+  tel_elm.innerHTML = tel;
+  job_elm.innerHTML = job;
+  address01_elm.innerHTML = address01;
+  address02_elm.innerHTML = address02;
+}
+
+function ConfirmForm_close() {
+  var fixButton_elm = document.getElementById("formConf-fadeIn");
+  fixButton_elm.classList.remove('show');
+  document.body.classList.remove('fixed','noscroll');
 }
